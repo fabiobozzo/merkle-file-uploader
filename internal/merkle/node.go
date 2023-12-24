e69e227ece4c8876b55fdce2b11a5497f9ac732f
Fabio Bozzo <fabio.bozzo@consensys.net>
2023-12-24 20:52:35 +0100

    fix typo

--- internal/merkle/node.go
+++ internal/merkle/node.go
@@ -1,7 +1,7 @@
 package merkle
 
-// Node contains data, representing the h of
-// the file or the combined h of its children,
+// Node contains data, representing the hash of
+// the file or the combined hash of its children,
 // and pointers to its left and right children.
 type Node struct {
 	Data  string
@@ -9,8 +9,8 @@ type Node struct {
 	Right *Node
 }
 
-// NewNode takes two nodes as input, h their data together,
-// and returns a new node with the resulting h.
+// NewNode takes two nodes as input, hash their data together,
+// and returns a new node with the resulting hash.
 func NewNode(left, right *Node, data string, hash HashFn) (n *Node) {
 	n = &Node{}
 
